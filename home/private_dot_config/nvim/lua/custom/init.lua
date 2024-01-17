67cb7ade751814c3361fcbf3ec048c3348b1ffb8
Wred <wredhyn@gmail.com>
2024-01-17 10:11:27 -0500

    Use nvim for merge and diff, add ctrl-q and ctrl-s for neovim, add ctrl-h/j/k/l for tmux moving, add java nvim plugin
    Signed-off-by: Wred <wredhyn@gmail.com>

--- home/private_dot_config/nvim/lua/custom/init.lua
+++ home/private_dot_config/nvim/lua/custom/init.lua
@@ -1,1 +1,7 @@
 vim.opt.colorcolumn = "80"
+
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-q>", "<cmd>qa<cr>", { desc = "Quit" })
+
+vim.wo.relativenumber = true
+
