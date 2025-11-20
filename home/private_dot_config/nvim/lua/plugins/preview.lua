0ddbe6e770a356200e018b977d9509f552eb4663
andre.milton <andre.milton@servicenow.com>
2025-11-20 10:15:03 -0500

    Added keyboard shortcuts for live-preview in nvim

--- home/private_dot_config/nvim/lua/plugins/preview.lua
+++ home/private_dot_config/nvim/lua/plugins/preview.lua
@@ -5,5 +5,21 @@ return {
 		dependencies = {
 			'nvim-telescope/telescope.nvim',
 		},
+		keys = {
+			{
+				'<leader>pp',
+				function()
+					vim.cmd('LivePreview close')
+					vim.cmd('LivePreview start')
+				end,
+				desc = 'Live Preview Start',
+			},
+			{
+				'<leader>pc',
+				'<cmd>LivePreview close<CR>',
+				desc = 'Live Preview Close',
+			},
+		},
+
 	},
 }
