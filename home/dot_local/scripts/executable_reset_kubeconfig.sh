69e4a2798194ebf0917da02134bc54189dd56c49
andre.milton <andre.milton@servicenow.com>
2026-03-13 12:50:30 -0400

    Fix kubeconfig reset for non-interactive

--- home/dot_local/scripts/executable_reset_kubeconfig.sh
+++ home/dot_local/scripts/executable_reset_kubeconfig.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Use all configs in Kubernetes config folder
-if [ -d "${HOME}/.kube" ]; then
+# Use all configs in Kubernetes config folder, only if KUBECONFIG is not already set
+if [ -z "${KUBECONFIG}" ] && [ -d "${HOME}/.kube" ]; then
 	export KUBECONFIG=$(ls ~/.kube/*config | paste -sd ":" -)
 fi
